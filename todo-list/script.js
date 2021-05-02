cfe107384289ce10b125e21072988906428086c1
Chris Paterson <chrispaterson22@gmail.com>
2021-05-02 12:03:43 +0100

    WIP JS

---
+++ todo-list/script.js
@@ -0,0 +1,23 @@
+const form = document.getElementById('form')
+const input = document.getElementById('input')
+const todosUl = document.getElementById('todos')
+
+form.addEventListener('submit', e => {
+	e.preventDefault()
+
+	addTodo()
+})
+
+function addTodo(todo) {
+	let todoText = input.value
+
+	if (todo) {
+		todoText = todo.text
+	}
+
+	if (todoText) {
+		const todoEl = document.createElement('li')
+		if (todo && todo) {
+		}
+	}
+}
